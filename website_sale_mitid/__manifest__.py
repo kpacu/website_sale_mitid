fe8e060e24d955f7fe14716ea6a7ea3509fe934b
kpacu <krassi.dimitrof@gmail.com>
2026-08-17 11:56:35 +0300

    Update __manifest__.py

--- website_sale_mitid/__manifest__.py
+++ website_sale_mitid/__manifest__.py
@@ -8,7 +8,7 @@
         Ensures compliance with age-verification requirements before payment processing.
     """,
     'license': 'OPL-1',
-    'price': 149.00,
+    'price': 1,
     'currency': 'EUR',
     'author': 'IntelligentSolutions',
     'website': 'https://github.com/kpacu/website_sale_mitid',
